0c5c5400e55ea89e304ed1f5b24fd41b71481460
Hrishabh.Duo <hrishabhdivya246@gmail.com>
2022-07-05 08:47:53 +0530

    configured it to get access to a cisco router

--- telnet.py
+++ telnet.py
@@ -1,19 +1,24 @@
 import getpass
 import telnetlib
-
-HOST = "localhost"
-user = input("Enter your remote account: ")
+#username avrage password telnet // enable password auto
+HOST = "10.0.0.1"
+user = input("Enter your telnet Username: ")
 password = getpass.getpass()
 
 tn = telnetlib.Telnet(HOST)
 
-tn.read_until(b"login: ")
+tn.read_until(b"Username: ")
 tn.write(user.encode('ascii') + b"\n")
 if password:
     tn.read_until(b"Password: ")
     tn.write(password.encode('ascii') + b"\n")
 
-tn.write(b"ls\n")
+tn.write(b"enable\n")
+tn.write(b"auto\n")
+tn.write(b"conf t\n")
+tn.write(b"int loop 0\n")
+tn.write(b"ip address 1.1.1.1 255.255.255.255\n")
+tn.write(b"end\n")
 tn.write(b"exit\n")
 
 print(tn.read_all().decode('ascii'))
